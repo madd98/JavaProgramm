08633154502d049d71919c345e9463eca0566863
madd98 <mamadou.diallo102031@gmail.com>
2022-09-21 22:45:09 +0200

    Second java Programm

---
+++ HelloAlpha.java
@@ -0,0 +1,10 @@
+public HelloAlpha {
+  public static void main (String agrv[]){
+       String nom = "Diallo";
+       String prenom = "Mamadou Alpha Djouhe";
+       System.out.println( nom + " " + prenom);
+       }
+ }
+
+
+
